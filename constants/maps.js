1318e987ba505ad79e424d1119882ad11c326826
WuangHuyyy <75169577+emsiqh@users.noreply.github.com>
2023-04-20 16:52:09 +0700

    Update maps.js

--- constants/maps.js
+++ constants/maps.js
@@ -1,3 +1,3 @@
-const GOOGLE_API_KEY = "AIzaSyBdcNudfNXB-VZEblSpn9nBqxNPQjycBtM"
-
+// const GOOGLE_API_KEY = "AIzaSyBdcNudfNXB-VZEblSpn9nBqxNPQjycBtM"
+const GOOGLE_API_KEY = "<API_KEY>"
 export default GOOGLE_API_KEY
